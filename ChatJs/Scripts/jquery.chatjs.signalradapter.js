68fe4dbd5451821b2084ac12812fedec3bf8b32c
André Rodrigues Pena <andrerpena@gmail.com>
2013-07-18 22:13:49 -0300

    ChatJS is not MIT Licensed
    I'm back and I decided to change ChatJS to MIT. It's gonna be better for
    everyone. Changes will come!

--- ChatJs/Scripts/jquery.chatjs.signalradapter.js
+++ ChatJs/Scripts/jquery.chatjs.signalradapter.js
@@ -1,19 +1,10 @@
 ﻿/**
- * ChatJS 1.0
+ * ChatJS 1.0 - MIT License
  * www.chatjs.net
  * 
  * Copyright (c) 2013, André Pena
  * All rights reserved.
- * 
- * Redistribution and use in source and binary forms,
- * with or without modification, are permitted provided
- * that the following conditions are met:
- * 
- *     - Redistributions of source code must retain the above copyright notice,
- *       this list of conditions and the following disclaimer.
- * 
- *     - The software containing ChatJS must not directly or indirectly charge the
- *       end user at any plan. Otherwise a commercial license is required.
+ *
  **/
 
 function SignalRAdapter() {
